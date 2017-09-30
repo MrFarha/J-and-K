8ca77de6933421ef80cd82a9c087feeab1322568
MrFarha <miko-inc@live.com>
2017-09-29 21:03:24 -0400

    first commit

---
+++ Libraries/Nop.Data/Mapping/Catalog/CategoryMap.cs
@@ -0,0 +1,24 @@
+using Nop.Core.Domain.Catalog;
+
+namespace Nop.Data.Mapping.Catalog
+{
+    public partial class CategoryMap : NopEntityTypeConfiguration<Category>
+    {
+        public CategoryMap()
+        {
+            this.ToTable("Category");
+            this.HasKey(c => c.Id);
+            this.Property(c => c.Name).IsRequired().HasMaxLength(400);
+            this.Property(c => c.MetaKeywords).HasMaxLength(400);
+            this.Property(c => c.MetaTitle).HasMaxLength(400);
+            this.Property(c => c.PriceRanges).HasMaxLength(400);
+            this.Property(c => c.PageSizeOptions).HasMaxLength(200);
+
+            // This code maps a column in the database to the new property we created above
+            // This creates a nullable nvarchar with a length of 255 characters 
+            // in the Category SQL table
+            this.Property(m => m.SomeNewProperty).HasMaxLength(255).IsOptional();
+
+        }
+    }
+}
